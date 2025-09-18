c36882d38ef17f11c9eda76ad87408808bd38a51
Hetarth Dave <davehetarth@gmail.com>
2025-09-18 14:25:49 -0400

    <Image> tage added

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Assignments() {
   return (
     <div id="wd-assignments">
@@ -10,26 +12,26 @@ export default function Assignments() {
       <ul id="wd-assignment-list">
         <div>
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A1 - ENV + HTML
-          </a> </li>
+          </Link> </li>
           <div>Multiple Modules|Not available until May 6 at 12:00am | Due May 13 at 11:59pm | 100pts</div>
           </div>
           <div>
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A2 - CSS and Bootstrap
-          </a> </li>
+          </Link> </li>
           <div>Multiple Modules|Not available until May 13 at 12:00am | Due May 20 at 11:59pm | 100pts</div>
           </div>
           <div>
         <li className="wd-assignment-list-item">
-          <a href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A3 - JAVASCRIPT AND REACT
-          </a> </li>
+          </Link> </li>
           <div>Multiple Modules|Not available until May 20 at 12:00am | Due May 27 at 11:59pm | 100pts</div>
           </div>
         
